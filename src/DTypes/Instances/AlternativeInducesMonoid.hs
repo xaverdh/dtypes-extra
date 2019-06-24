93b80b01d6a6a02c1fd3dc585cb554d8f29d944f
Dominik Xaver Hörl <hoe.dom@gmx.de>
2019-06-24 15:02:45 +0200

    semigroup monoid proposal

--- src/DTypes/Instances/AlternativeInducesMonoid.hs
+++ src/DTypes/Instances/AlternativeInducesMonoid.hs
@@ -4,7 +4,9 @@ module DTypes.Instances.AlternativeInducesMonoid where
 import DTypes.Classes.DApplicative
 import Control.Applicative
 
+instance (DApplicative d,Alternative f) => Semigroup (d f) where
+  (<>) = dliftA2 (<|>)
+
 instance (DApplicative d,Alternative f) => Monoid (d f) where
   mempty = dpure empty
-  mappend = dliftA2 (<|>)
 
